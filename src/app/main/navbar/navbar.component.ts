2e42ff9e2a75442811c8995195a91914aa4cd0d0
John Wu <john.wu@mail.mcgill.ca>
2017-10-22 23:02:48 -0400

    Got title working on nav bar

--- src/app/main/navbar/navbar.component.ts
+++ src/app/main/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
 import {Router} from "@angular/router";
 import {ROUTES} from "./navbar-routes.config";
 
@@ -10,17 +10,49 @@ import {ROUTES} from "./navbar-routes.config";
 export class NavbarComponent implements OnInit {
 
   public menuItems: any = []
+  private toggleButton: any
+  private sidebarVisible: boolean
 
-  constructor(
-    private router: Router
-  ) {}
+  constructor( private router: Router, private element: ElementRef) {
+    this.sidebarVisible = false
+  }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem)
+    const navbar: HTMLElement = this.element.nativeElement
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggle')[0]
   }
 
   public isActive(url: any): boolean {
     return this.router.url.includes(url)
   }
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const body = document.getElementsByTagName('body')[0];
+    setTimeout(function(){
+      toggleButton.classList.add('toggled');
+    }, 500);
+    body.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  }
+
+  sidebarClose() {
+    const body = document.getElementsByTagName('body')[0];
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    body.classList.remove('nav-open');
+  }
+
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+  };
 
 }
